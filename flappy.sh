cc86a3e5d794a132b221c55eb55005cac1d03728
Hubert <kiszkahub@gmail.com>
2025-01-18 21:38:39 +0100

    Walls works

--- flappy.sh
+++ flappy.sh
@@ -2,11 +2,14 @@
 MAX_WIDTH=190
 MAX_HEIGHT=44
 IN_COL_GAP=10
+COLUMN_WIDTH=2
 declare -A arr
-
+declare -A birdArr
 for((i=0; i<MAX_HEIGHT; i++)); do
     for((j=0; j<MAX_WIDTH; j++)); do
         arr[$i,$j]=' '
+        arr[0,$j]='@'
+        arr[$((MAX_HEIGHT-1)),$j]='@'
     done
 done
 print_board(){
@@ -19,25 +22,46 @@ print_board(){
     done
 }
 print_board
-
 GAP=0
+column_counter=0
+
 while true; do
-    for((i=0; i<MAX_HEIGHT; i++)); do
-        arr[$i,$((MAX_WIDTH-1))]=' '
-    done
-    for((i=0; i<MAX_HEIGHT; i++));do
-        for((j=0; j<MAX_WIDTH-1; j++));do
-            arr[$i,$j]=${arr[$i,$((j+1))]}
+    for((i=1; i<MAX_HEIGHT-1; i++)); do
+        for((j=0; j<MAX_WIDTH-COLUMN_WIDTH; j++)); do
+            arr[$i,$j]=${arr[$i,$((j+COLUMN_WIDTH))]}
         done
     done
-    rand_height=$((5+RANDOM%25))
-    for((i=0; i<MAX_HEIGHT; i++)); do
-        if((i<rand_height || i>=rand_height+IN_COL_GAP)); then
-            arr[$i,$((MAX_WIDTH-1))]='*'
-        else
-            arr[$i,$((MAX_WIDTH-1))]=' '
-        fi
+    for((i=1; i<MAX_HEIGHT-1; i++)); do
+        for((j=0; j<COLUMN_WIDTH; j++)); do
+            unset arr[$i,$j]
+        done
+    done
+    if((column_counter >= IN_COL_GAP)); then
+        rand_height=$((5+RANDOM%25))
+        for((i=1; i<MAX_HEIGHT-1; i++)); do
+            if((i<rand_height || i>=rand_height+IN_COL_GAP)); then
+                for((j=0; j<COLUMN_WIDTH; j++)); do
+                    arr[$i,$((MAX_WIDTH-COLUMN_WIDTH+j))]='*'
+                done
+            else
+                for((j=0; j<COLUMN_WIDTH; j++)); do
+                    arr[$i,$((MAX_WIDTH-COLUMN_WIDTH+j))]=' '
+                done
+            fi
+        done
+        column_counter=0
+    else
+        for((i=1; i<MAX_HEIGHT-1; i++)); do
+            for((w=0; w<COLUMN_WIDTH; w++)); do
+                arr[$i,$((MAX_WIDTH-COLUMN_WIDTH+w))]=' '
+            done
+        done
+        column_counter=$((column_counter+1))
+    fi
+    for((j=0; j<MAX_WIDTH; j++)); do
+        arr[0,$j]='@'
+        arr[$((MAX_HEIGHT-1)),$j]='@'
     done
     print_board
-    sleep 1
+    sleep 0.1
 done
